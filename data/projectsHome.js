94885370a2781b5588b8fda7e158442a1e7aee9f
GuneetBola <guneetbola@gmail.com>
2021-01-08 19:58:57 -0500

    Add files via upload

---
+++ data/projectsHome.js
@@ -0,0 +1,27 @@
+import project1 from '../assets/project1.png'
+import project3 from '../assets/project3.png'
+
+const PROJECTSHOME = [
+    {
+        id: 1,
+        title: 'Artist Searcher',
+        description: 'A React App that I built involving JS and core web dev concepts',
+        link1: 'https://artistsearcher.netlify.app/',
+        link2: 'https://github.com/GuneetBola/Artist-Search',
+        button1:'Go to Site',
+        button2:'GitHub',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'Stock Trader-Finance',
+        description: 'A React App that I built involving JS and core web dev concepts',
+        link1: 'https://youtu.be/rnLiO9T7Z8g', 
+        link2: 'https://github.com/GuneetBola/Stock-Trader-CS50',
+        button1:'Go to YouTube Video',
+        button2:'GitHub',
+        image: project3
+    }
+];
+
+export default PROJECTSHOME;
---
+++ data/socialProfiles.js
@@ -0,0 +1,31 @@
+import linkedinIcon from '../assets/linkedin_icon.png'
+import githubIcon from '../assets/github_icon.png'
+import emailIcon from '../assets/email_icon.png'
+
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        title: 'Email',
+        link: 'mailto:gbola@uwaterloo.ca',
+        image: emailIcon
+
+    },
+
+    {
+        id: 2,
+        title: 'Linkedin',
+        link: "https://www.linkedin.com/in/guneet-bola-77ab821a7/",
+        image: linkedinIcon
+
+    },
+    {
+        id: 3,
+        title: 'Github',
+        link: "https://github.com/GuneetBola",
+        image: githubIcon
+
+    }
+]
+
+export default SOCIAL_PROFILES;
