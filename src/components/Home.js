7e893bcdbd8b0b858a2ae9c408ee7704bd66685a
GuneetBola <guneetbola@gmail.com>
2021-06-20 20:00:30 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -37,7 +37,7 @@ class Home extends Component {
                         <p>Feel free to contact me by scrolling to the bottom of the page and clicking on the icons.</p>
                         <hr />
                         <p>Currently, I am a Honors Computer Science Student at the Univeristy of Waterloo and I working as a Developer for WIL programs at the University of Waterloo. Also, I am activley seeking Summer 2022 internships.</p>
-                        <button class="btn justifiy-content-centre" href={resume} rel="noopener" target="_blank">Resume</button>
+                        <button class="btn" href={resume} rel="noopener" target="_blank">Resume</button>
                     </Jumbotron>
                 </div>
                 
